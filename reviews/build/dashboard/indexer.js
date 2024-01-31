9635ada3e0f6a0b443d967176b019e85989c2157
vid <vid@users.noreply.github.com>
2024-01-31 16:36:17 +0000

    update e2e test reviews

--- reviews/build/dashboard/indexer.js
+++ reviews/build/dashboard/indexer.js
@@ -1,2 +1,2 @@
 export const endpoint = "./tracks/";
-export async function getPublishedReviews() { return ["1706563270240-tracksHistory.json","1706653375921-tracksHistory.json","1706654689478-tracksHistory.json"]; }
+export async function getPublishedReviews() { return ["1706653375921-tracksHistory.json","1706654689478-tracksHistory.json","1706718976550-tracksHistory.json"]; }
